eb4b006be72b2627c53c6f5d5b2437e81d0fec90
James Anderson <jsondev1.0@outlook.com>
2017-12-20 00:46:40 -0500

    Module 3.6 - Using Template Variables

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent, EventListComponent],
+    declarations: [EventsAppComponent, EventListComponent, EventThumbnailComponent],
     bootstrap: [EventsAppComponent]
 })
 
