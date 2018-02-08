2e6486e16050dbd3e3b54193c9437ab0a9ff6b87
James Anderson <jsondev1.0@outlook.com>
2018-02-08 15:21:17 -0500

    Beginning Routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,12 @@ import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './shared/event.service';
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent, EventListComponent, EventThumbnailComponent, NavBarComponent],
+    providers: [EventService],
     bootstrap: [EventsAppComponent]
 })
 
---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+ templateUrl: '/app/events/event-details/event-details.html'
+})
+
+export class EventDetailsComponent {
+
+}
